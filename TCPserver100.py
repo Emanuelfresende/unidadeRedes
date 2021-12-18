7b0108729eae2409f6e05ead556ba9e7d032254a
Emanuelfresende <86726170+Emanuelfresende@users.noreply.github.com>
2021-12-18 11:06:12 -0300

    Update TCPserver100.py
    att -v final

--- TCPserver100.py
+++ TCPserver100.py
@@ -1,4 +1,5 @@
 import socket
+import time
 import string
 from datetime import datetime
 import os
@@ -14,16 +15,22 @@ orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
 
+count = 0
+
 while True:
     con, client = tcp.accept()
     print('conectado por', client)
     while True:
-        request = con.recv(1024).decode("utf-8")
-        if not request: break
+        try:
+            request = con.recv(1024).decode("utf-8")
+        except ConnectionResetError:
+            break
+        if not request:
+            break
         first_line = request.split('\r\n')[0]
         file_path = first_line.split(' ', 2)[1]
 
-        print('file path: ', file_path)
+        #print('file path: ', file_path)
 
         if file_path == "/":
             file_path = "/response.html"
@@ -44,7 +51,7 @@ while True:
                     with open('./error.html') as f:
                         file_ = f.readlines()
                 except IOError:
-                    pass
+                    pass 
                 response = 'HTTP/1.1 404 Not Found\r\n'
                 response += 'Date: {}\r\n'.format(now_s)
                 response += 'Server: server\r\n'
@@ -63,48 +70,19 @@ while True:
         else:
             now = datetime.now()
             now_s = now.strftime("%a, %-d %b %Y %H:%M:%S %Z")
-            url_field = request.split('\r\n')[3]
-            url = url_field.split(' ')[1]
-            busca_palavra = url_field.split(' ')[2]
-            print(url)
-            print(busca_palavra)
+            field_handler = request.split('\r\n')[3]
+            field = field_handler.split(': ')[0]
+            url = field_handler.split(' ')[1]
+            #print(field_handler)
+            #print(field)
+            #print(url)
             if not url:
                 size_err = os.path.getsize('./error.html')
                 try:
                     with open('./error.html') as f:
                         file_ = f.readlines()
                 except IOError:
-                    pass
-                response = 'HTTP/1.1 404 Not Found\r\n'
-                response += 'Date: {}\r\n'.format(now_s)
-                response += 'Server: server\r\n'
-                response += 'Content-Length: {}\r\n'.format(size_err)
-                response += 'Content-Type: text/html\r\n\r\n'
-                response += ''.join(file_)
-            else:
-                size_file = os.path.getsize('.{}'.format(file_path))
-                response = 'HTTP/1.1 200 OK\r\n'
-                response += 'Date: {}\r\n'.format(now_s)
-                response += 'Server: server\r\n'
-                response += 'Content-Length: {}\r\n'.format(size_file)
-                response += 'Content-Type: text/html\r\n\r\n'
-                response += ''.join(file_)
-
-        else:
-            now = datetime.now()
-            now_s = now.strftime("%a, %-d %b %Y %H:%M:%S %Z")
-            url_field = request.split('\r\n')[3]
-            url = url_field.split(' ')[1]
-            busca_palavra = url_field.split(' ')[2]
-            print(url)
-            print(busca_palavra)
-            if not url:
-                size_err = os.path.getsize('./error.html')
-                try:
-                    with open('./error.html') as f:
-                        file_ = f.readlines()
-                except IOError:
-                    pass
+                    pass 
                 response = 'HTTP/Tupi 404 Not Found\r\n'
                 response += 'Date: {}\r\n'.format(now_s)
                 response += 'Server: server\r\n'
@@ -117,26 +95,36 @@ while True:
                 response += 'Server: server\r\n'
                 response += 'Content-Type: text/plain\r\n\r\n'
 
-                #url = "https://ccsa.ufs.br/pagina/20147-departamento-de-relacoes-internacionais
-
                 page = requests.get(url)
                 if page.status_code != 200:
-                    print('Um problema ocorreu e a requisição retornou código ', $
-                    sys.exit(1)
+                    response += 'Um problema ocorreu e a requisição retornou código {}'.format(page.status_code)
+                    #print('Um problema ocorreu e a requisição retornou código ', page.status_code)
+                    field = ''
 
                 data = page.text
                 soup = BeautifulSoup(data, features="html.parser")
 
-                #soup.find_all(string=re.compile("UFS"))
-                matches = soup.find_all(string=re.compile(busca_palavra))
-                count = 0
-                for nome in matches:
-                     print(nome)
-                     count = count + 1
-                print("\n-- NUMERO DE OCORRENCIAS {}--".format(count))
-
-        con.send(bytes(response, "utf-8"))
+                elif field == "URL_Busca":
+                    busca_palavra = field_handler.split(' ')[2]
+                    pal= "----{}----".format(busca_palavra)
+                    #print("----{}----".format(busca_palavra))
+                    response += pal+'\n'
+                    matches = soup.find_all(string=re.compile(busca_palavra))
+                    count = 0
+                    for nome in matches:
+                        #print(nome)
+                        response += nome+'\n'
+                        count = count + 1
+                    occ = "\n-- NUMERO DE OCORRENCIAS {}--".format(count)
+                    #print("\n-- NUMERO DE OCORRENCIAS {}--".format(count))
+                    response += occ+'\n'
+                    
+                
+        encoded = bytes(response, "utf-8")
+        byte_count = bytes(str(len(encoded)), "utf-8")
+        con.send(byte_count)
+        time.sleep(0.1)
+        con.send(encoded)
 
     print('finalizando conexao com cliente', client)
     con.close()
-            
